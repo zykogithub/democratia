bb73f319c907e7de7713db105a65ae75864690a7
zykogithub <modaherry@gmail.com>
2025-04-17 20:55:26 +0200

    factorisation du composant Header, création et implémentation du composant EntryComponent, page de création de compte fini

---
+++ com.democratia/Views/Component/EntryComponent.cs
@@ -0,0 +1,91 @@
+namespace com.democratia.Views.Component;
+using com.democratia.Resources.Styles;
+
+
+public partial class EntryComponent : ContentView
+{
+	private string _title;
+    private string _valeurDonne;
+    private bool _passWord;
+
+
+    // Propriété publique pour passer un paramètre
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            _title = value;
+            // Met à jour le texte du Label si nécessaire
+            if (Content is VerticalStackLayout layout && layout.Children[0] is Label label)
+            {
+                label.Text = _title;
+            }
+        }
+    }
+    public string valeurDonne
+    {
+        get => _valeurDonne;
+        set
+        {
+            
+            // Met à jour le paramètre si nécessaire
+            if (Content is VerticalStackLayout layout && layout.Children[2] is Entry)
+            {
+                _valeurDonne = value;
+            }
+        }
+    }
+    public bool? passWord
+    {
+        get => _passWord;
+        set
+        {
+            _passWord = (bool)value;
+            // Met à jour le texte du Label si nécessaire
+            if (Content is VerticalStackLayout layout && layout.Children[2] is Entry entry)
+            {
+                entry.IsPassword = _passWord;
+            }
+        }
+    }
+    public EntryComponent()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label {
+
+
+                    HorizontalOptions = LayoutOptions.Center,
+                    Text = _title,
+					Style = (Style)Application.Current.Resources["SubHeadlineStyle"]
+				},
+				new BoxView
+				{
+					HeightRequest=30
+				},
+				new Entry
+				{
+                    Style = (Style)Application.Current.Resources["EntryStyle"],
+                    MaximumWidthRequest=300
+                },
+                new BoxView
+                {
+                    HeightRequest=30
+                }
+
+            }
+		};
+	}
+    private void OnTitleChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is EntryComponent component && component.Content is VerticalStackLayout layout)
+        {
+            if (layout.Children[2] is Entry entry )
+            {
+                this._valeurDonne = (string)newValue;
+            }
+        }
+    }
+}
